612af176b8fa71bb2cb91988d4d6000dc2231a39
Josh Choo <dev@joshuous.com>
2019-01-25 23:21:23 +0800

    sched/fair: Add bias schedtune boosted tasks sched feature
    Schedtune boosted tasks are biased to higher capacity CPUs by default.
    Add a sched feature to enable/disable this behaviour.

--- kernel/sched/fair.c
+++ kernel/sched/fair.c
@@ -7107,6 +7107,9 @@ static int start_cpu(bool boosted)
 {
 	struct root_domain *rd = cpu_rq(smp_processor_id())->rd;
 
+	if(!sched_feat(STUNE_BOOST_BIAS_BIG))
+		return rd->min_cap_orig_cpu;
+
 	return (boosted && st_cpu_bias) ? rd->max_cap_orig_cpu : rd->min_cap_orig_cpu;
 }
 
