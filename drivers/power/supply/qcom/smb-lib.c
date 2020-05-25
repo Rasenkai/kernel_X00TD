KudProject/kernel_asus_sdm660

X00T: power: qcom: Remove country-based adapter checking
* This is disgusting to the core.
* Make it assume that it's 2A charger if other adapter is detected.
* Clean up a bit.

Signed-off-by: Albert I <krascgq@outlook.co.id>

drivers/power/supply/qcom/smb-lib.c
@@ -78,7 +78,6 @@ bool smartchg_stop_flag;
 extern int charger_limit_enable_flag;
 extern int charger_limit_value;
 static bool asus_flow_processing;
-extern int BR_countrycode;
 int asus_get_prop_batt_temp(struct smb_charger *chg);
 int asus_get_prop_batt_volt(struct smb_charger *chg);
 int asus_get_prop_batt_capacity(struct smb_charger *chg);
@@ -3765,10 +3764,9 @@ void jeita_rule(void)
 	bat_volt = asus_get_prop_batt_volt(smbchg_dev);
 	bat_capacity = asus_get_prop_batt_capacity(smbchg_dev);
 	state = smbchg_jeita_judge_state(state, bat_temp);
-	pr_debug("%s: state=%d, batt_health = %s, bat_temp = %d, bat_volt = %d, bat_capacity=%d, ICL = 0x%x, FV_reg=0x%x, Countrycode %d\n",
+	pr_debug("%s: state=%d, batt_health = %s, bat_temp = %d, bat_volt = %d, bat_capacity=%d, ICL = 0x%x, FV_reg=0x%x\n",
 			__func__, state, health_type[bat_health], bat_temp,
-			bat_volt, bat_capacity, ICL_reg, FV_reg,
-			BR_countrycode);
+			bat_volt, bat_capacity, ICL_reg, FV_reg);
 
 	switch (state) {
 	case JEITA_STATE_LESS_THAN_0:
@@ -4109,24 +4107,10 @@ void asus_adapter_adc_work(struct work_struct *work)
 	/* determine current-setting value for DCP type AC: */
 	switch (ASUS_ADAPTER_ID) {
 	case ASUS_750K:
-		usb_max_current = ICL_2000mA;
-		break;
-
 	case ASUS_200K:
-		usb_max_current = ICL_2000mA;
-		break;
-
 	case PB:
-		usb_max_current = ICL_2000mA;
-		break;
-
 	case OTHERS:
-		if (BR_countrycode == COUNTRY_BR ||
-			BR_countrycode == COUNTRY_IN)
-			usb_max_current = ICL_2000mA;
-		else
-			usb_max_current = ICL_1000mA;
-
+		usb_max_current = ICL_2000mA;
 		break;
 
 	case ADC_NOT_READY:

Rasenkai/kernel_X00TD

Signed-off-by: ajaivasudeve <ajaivasudeve@gmail.com>

drivers/power/supply/qcom/smb-lib.c
@@ -78,7 +78,6 @@ bool smartchg_stop_flag;
 extern int charger_limit_enable_flag;
 extern int charger_limit_value;
 static bool asus_flow_processing;
-extern int BR_countrycode;
 int asus_get_prop_batt_temp(struct smb_charger *chg);
 int asus_get_prop_batt_volt(struct smb_charger *chg);
 int asus_get_prop_batt_capacity(struct smb_charger *chg);
@@ -3764,10 +3763,9 @@ void jeita_rule(void)
 	bat_volt = asus_get_prop_batt_volt(smbchg_dev);
 	bat_capacity = asus_get_prop_batt_capacity(smbchg_dev);
 	state = smbchg_jeita_judge_state(state, bat_temp);
-	pr_debug("%s: state=%d, batt_health = %s, bat_temp = %d, bat_volt = %d, bat_capacity=%d, ICL = 0x%x, FV_reg=0x%x, Countrycode %d\n",
+	pr_debug("%s: state=%d, batt_health = %s, bat_temp = %d, bat_volt = %d, bat_capacity=%d, ICL = 0x%x, FV_reg=0x%x\n",
 			__func__, state, health_type[bat_health], bat_temp,
-			bat_volt, bat_capacity, ICL_reg, FV_reg,
-			BR_countrycode);
+			bat_volt, bat_capacity, ICL_reg, FV_reg);
 
 	switch (state) {
 	case JEITA_STATE_LESS_THAN_0:
@@ -4108,24 +4106,10 @@ void asus_adapter_adc_work(struct work_struct *work)
 	/* determine current-setting value for DCP type AC: */
 	switch (ASUS_ADAPTER_ID) {
 	case ASUS_750K:
-		usb_max_current = ICL_2050mA;
-		break;
-
 	case ASUS_200K:
-		usb_max_current = ICL_2050mA;
-		break;
-
 	case PB:
-		usb_max_current = ICL_2050mA;
-		break;
-
 	case OTHERS:
-		if (BR_countrycode == COUNTRY_BR ||
-			BR_countrycode == COUNTRY_IN)
-			usb_max_current = ICL_2050mA;
-		else
-			usb_max_current = ICL_1000mA;
-
+		usb_max_current = ICL_2050mA;
 		break;
 
 	case ADC_NOT_READY: